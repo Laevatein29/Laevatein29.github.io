c2172ebb98fc4a19fa055bd85d0620575df509d2
laevatein29 <liyongchen29@gmail.com>
2024-11-13 17:12:27 +0800

    feat: update dependencies

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,43 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { routes } from '@generouted/react-router'
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import { createHashRouter, RouterProvider } from 'react-router-dom'
+// import '../i18n'
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+const router = createHashRouter(routes)
+
+const future = {
+
+  v7_startTransition: true,
+  v7_normalizeFormMethod: true,
+  v7_partialHydration: true,
+  v7_relativeSplatPath: true,
+  v7_skipActionErrorRevalidation: true,
+} as any
+
+// eslint-disable-next-line react-refresh/only-export-components
+function Routes() {
+  return (
+    <RouterProvider
+      router={router}
+      future={future}
+    />
+  )
+}
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <Routes></Routes>
+  </React.StrictMode>,
 )
+// INFO: use following code to build app without hash router
+//
+// import React from 'react'
+// import ReactDOM from 'react-dom/client'
+// import { Routes } from '@generouted/react-router'
+//
+// ReactDOM.createRoot(document.getElementById('root')!).render(
+//   <React.StrictMode>
+//     <Routes></Routes>
+//   </React.StrictMode>,
+// )
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,32 @@
+import { resolve } from 'node:path'
+import generouted from '@generouted/react-router/plugin'
 import react from '@vitejs/plugin-react-swc'
+import AutoImport from 'unplugin-auto-import/vite'
 import { defineConfig } from 'vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    generouted(),
+    AutoImport({
+      imports: ['react', 'react-router-dom'],
+      dts: './auto-imports.d.ts',
+      dirs: ['src/components'],
+    }),
+  ],
+  css: {
+    preprocessorOptions: {
+      scss: {
+        additionalData: `
+            @import "./src/styles/main.scss";
+          `,
+      },
+    },
+  },
+  resolve: {
+    alias: {
+      '@': resolve(__dirname, './src'),
+    },
+  },
 })
